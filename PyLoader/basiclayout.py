57a3d85cfcabee3f09057a0bea92acf8585a6344
martin-montas <martinmontas1@gmail.com>
2024-12-05 09:53:16 -0500

    Added a new upper separator for future dev

--- PyLoader/basiclayout.py
+++ PyLoader/basiclayout.py
@@ -1,7 +1,6 @@
 import tkinter                  as tk
 from tkinter                    import messagebox
 from PyLoader.sendButton        import SendButton
-#from PyLoader.urlEntry          import UrlEntry
 from PyLoader.requestBox        import RequestBox
 from PyLoader.topMenu           import TopDefaultMenu
 from PyLoader.rightFrameInput   import RightFrameInput 
@@ -10,7 +9,7 @@ class InitialLayout:
   '''
         Initiatests the basic layout
 
-        @param root - the root window
+        @param root - the default window
         @return None
   '''
   def __init__(self, root):
@@ -30,6 +29,7 @@ class InitialLayout:
 
     RequestBox(self.root)
     RightFrameInput(self.root)
+    TopDefaultMenu(self.root)
 
     # clear button for the RequestBox
     SendButton(
@@ -42,13 +42,11 @@ class InitialLayout:
     self.root, "Paste",
     self.button_bg, "white",
     x=(400 - self.button_rel), y=709, width=10, height=0)
-    # UrlEntry(self.root, width=30)
 
     footer = tk.Label(self.root, text=self.footer_version,
     bg=self.bg, fg=self.fg)
     footer.pack(side="bottom")
 
-    TopDefaultMenu(self.root)
 
   def option_selected(self, value):
      messagebox.showinfo("Selected Option", value)
--- PyLoader/topMenu.py
+++ PyLoader/topMenu.py
@@ -1,46 +1,21 @@
 import tkinter as tk
-#from tkinter import messagebox
+from tkinter import ttk
 
 class TopDefaultMenu:
-  '''
-  '''
-  def __init__(self, root, bg=None, fg=None):
-    self.root           = root
-    self.bg             = "lightgrey"
-    self.fg             = "black"
-    self.button_width   = 10
-    self.button_height  = 0
-    menu_frame          = tk.Frame(self.root, bg=self.bg)
-    self.result_frame   = tk.Frame(self.root, bg=self.bg)
-    self.result_label   = tk.Label(self.result_frame, font=("Arial", 16), 
-    bg=self.bg, fg=self.fg)
-    self.result_frame.pack(pady=30)
-    self.result_label.pack()
+    '''
+        Class to initialize the top menu
+        for PyLoader
+        @param root - the default window
+        @bg - background color
+        @fg - foreground color
+        @return None
+    '''
+    def __init__(self, root, bg=None, fg=None):
+        self.root           = root
+        self.bg             = "lightgrey"
+        self.fg             = "black"
+        self.button_width   = 10
+        self.button_height  = 0
 
-    # Create a frame for the horizontal menu (buttons)
-    menu_frame.pack(side=tk.TOP, fill=tk.X)
-    
-    # Define the menu buttons
-    menu_options = ["Seeker", "shooter", "Blah", "Nothing"]
-    for option in menu_options:
-        button = tk.Button(menu_frame, text=option, font=("Arial", 16), 
-        command=lambda opt=option: self.show_content(opt),
-        bg=self.bg, fg=self.fg,
-        width=self.button_width, height=self.button_height
-        )
-        button.pack(side=tk.LEFT, padx=10)
-
-  def show_content(self,option):
-
-    self.result_label.config(text="")
-    if option == "Option 1":
-        # TODO: called the classes here
-        # make sure you pass the proper elements to it
-        self.result_label.config(text="You selected Option 1: Information about Option 1.")
-    elif option == "Option 2":
-        self.result_label.config(text="You selected Option 2: Information about Option 2.")
-    elif option == "Option 3":
-        self.result_label.config(text="You selected Option 3: Information about Option 3.")
-    elif option == "Option 4":
-        self.result_label.config(text="You selected Option 4: Information about Option 4.")
-
+        horizontal_separator = ttk.Separator(self.root, orient="horizontal")
+        horizontal_separator.pack(fill="x", pady=75)
